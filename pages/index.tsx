1950a68b1e08866933c003a89170fd1a4f710bd8
allyn ralf ledesma <dgtlmonsieur@gmail.com>
2024-11-30 13:28:26 +0800

    initialized repo

---
+++ pages/index.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { useChat } from "ai/react";
+
+import Messages from "@/app/components/messages";
+import InputForm from "@/app/components/inputForm";
+
+export default function Home() {
+  const { messages, input, handleInputChange, handleSubmit, isLoading, stop } =
+    useChat({
+      api: "api/genai",
+    });
+
+  return (
+    <main className="flex min-h-screen flex-col items-center p-12 text-lg">
+      <InputForm
+        input={input}
+        handleInputChange={handleInputChange}
+        handleSubmit={handleSubmit}
+        isLoading={isLoading}
+        stop={stop}
+      />
+      <Messages messages={messages} isLoading={isLoading} />
+    </main>
+  );
+}
---
+++ pages/verify.tsx
@@ -0,0 +1,3 @@
+export default function Verify() {
+  return <h1>Hello, Verify Page</h1>
+}
